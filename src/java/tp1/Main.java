33b651885a5f887aea11b20476690dceddbbd218
L3-MIAGE-Samuel <samuelkervarec@gmail.com>
2018-10-10 13:56:50 +0200

    Initial commit

---
+++ src/java/tp1/Main.java
@@ -0,0 +1,21 @@
+package tp1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author samuel
+ */
+public class Main {
+    
+    public static void main(String [] args)
+	{
+		DAO d = new DAO();
+                d.test();
+	}
+    
+}
